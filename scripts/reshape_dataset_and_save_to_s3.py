8b0376e6ba8a3a6c7c23cea62513392270d2db63
hssn-20 <69965978+hssn-20@users.noreply.github.com>
2023-10-19 23:23:23 +0000

    reshaping script

--- scripts/reshape_dataset_and_save_to_s3.py
+++ scripts/reshape_dataset_and_save_to_s3.py
@@ -1,14 +1,31 @@
-import boto3
-from datasets import load_dataset_builder
+from datasets import load_dataset
+from datasets import concatenate_datasets
 
-# Create an S3 client
-s3_client = boto3.client('s3')
+## load the partial datasets and concatenate them
+for i in range(0, 78):
+    try:
+        dataset = load_dataset(f"Hack90/ncbi_genbank_part_{i}")
+    except:
+        print(f"Failed to load dataset {i}")
+        continue
+    if i == 1:
+        full_dataset = dataset
+    else:
+        full_dataset = concatenate_datasets([full_dataset, dataset])
 
-# Set up your storage options
-# Note: You may not need this depending on your subsequent code.
-storage_options = {"client": s3_client}
+## save the full dataset to s3
+full_dataset.save_to_disk("s3://dna_llm/ncbi_genbank_full")
 
-# Rest of your code remains similar...
-builder = load_dataset_builder("Hack90/virus_dna_dataset")
-output_dir = "s3://dna-llm/"
-builder.download_and_prepare(output_dir, storage_options=storage_options, file_format="parquet")
+## filter the dataset to only include sequences with length 2000
+full_dataset = full_dataset.filter(lambda example: example["seq_length"] == 2000)
+
+## reshape sequences to batches of 2000
+full_dataset = full_dataset.map(lambda example: {"sequence": [example["sequence"][i:i+2000] for i in range(0, len(example["sequence"]), 2000)]})
+
+full_dataset.save_to_disk("s3://dna_llm/ncbi_genbank_full_2000")
+
+
+
+
+
+
