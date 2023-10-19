e50d6ad9c825fc2229d8140da630add264f1811a
hssn-20 <69965978+hssn-20@users.noreply.github.com>
2023-10-19 23:36:06 +0000

    chunks

--- scripts/reshape_dataset_and_save_to_s3.py
+++ scripts/reshape_dataset_and_save_to_s3.py
@@ -19,10 +19,17 @@ full_dataset.save_to_disk("s3://dna_llm/ncbi_genbank_full")
 ## filter the dataset to only include sequences with length 2000
 full_dataset = full_dataset.filter(lambda example: example["seq_length"] == 2000)
 
-## reshape sequences to batches of 2000
-full_dataset = full_dataset.map(lambda example: {"sequence": [example["sequence"][i:i+2000] for i in range(0, len(example["sequence"]), 2000)]})
+## reshape sequences to batches of 2000 characters
+def chunk_sequences(examples):
+    chunks = []
+    for sentence in examples['sequence']:
+        chunks += [sentence[i:i + 2000] for i in range(0, len(sentence), 2000)]
+    return {'chunks': chunks}
 
-full_dataset.save_to_disk("s3://dna_llm/ncbi_genbank_full_2000")
+full_dataset = full_dataset.map(chunk_sequences, batched=True, batch_size=1000)
+full_dataset.save_to_disk("s3://dna_llm/ncbi_genbank_full_2000_bp_chunks")
+
+
 
 
 
