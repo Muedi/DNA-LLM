8f2bc6b01981aa00938735800d22ea6d6530d9c7
hssn-20 <69965978+hssn-20@users.noreply.github.com>
2023-10-19 22:23:19 +0000

    dataset

--- scripts/reshape_dataset_and_save_to_s3.py
+++ scripts/reshape_dataset_and_save_to_s3.py
@@ -1,13 +1,14 @@
-from botocore.session import Session
-import s3fs
+import boto3
 from datasets import load_dataset_builder
-storage_options = {"key": "XXX",
 
-"secret": "XXX"}
-s3_session = Session(profile="your_profile_name")
+# Create an S3 client
+s3_client = boto3.client('s3')
 
-storage_options = {"session": s3_session}
-fs = s3fs.S3FileSystem(**storage_options)
-builder = load_dataset_builder("your dataset id")
-output_dir = "s3://path/to/the/bucket/"
+# Set up your storage options
+# Note: You may not need this depending on your subsequent code.
+storage_options = {"client": s3_client}
+
+# Rest of your code remains similar...
+builder = load_dataset_builder("Hack90/virus_dna_dataset")
+output_dir = "s3://dna-llm/"
 builder.download_and_prepare(output_dir, storage_options=storage_options, file_format="parquet")
